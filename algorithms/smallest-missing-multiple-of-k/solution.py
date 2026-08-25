e4f2046cc909a4136995a605e318141f92990cc8
Alok Prakhar <prakharsitare0879@gmail.com>
2026-08-25 19:53:52 +0530

    feat: add Smallest Missing Multiple of K solution

--- algorithms/smallest-missing-multiple-of-k/solution.py
+++ algorithms/smallest-missing-multiple-of-k/solution.py
@@ -1,13 +1,9 @@
 class Solution:
-    def checkDivisibility(self, n: int) -> bool:
-        original = n
-        s = 0
-        m = 1
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        nums = set(nums)
+        multiple = k
 
-        while n > 0:
-            digit = n % 10
-            s += digit
-            m *= digit
-            n //= 10
+        while multiple in nums:
+            multiple += k
 
-        return original % (s + m) == 0
+        return multiple
